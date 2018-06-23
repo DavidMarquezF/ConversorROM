64136ff995e07ba31695ba81acc85886d29c2494
David <dmarquez.estudis@gmail.com>
2018-06-23 11:57:27 +0200

    creador cicles

--- src/VHDLSintetitzadorNotes/CreadorDeCiclesNecessaris.py
+++ src/VHDLSintetitzadorNotes/CreadorDeCiclesNecessaris.py
@@ -8,8 +8,11 @@ Creador de cicles
 Aquest mòdul s'encarrega de printejar els cicles que es necessiten per generar les freqüències necessàries per fer
 sonar les notes. Amb aquest sistema es pot arribar a generar totes les freqüències necessàries.
 
-Els cicles són iguals que els que es poden veure en el fitxer penjat a Atenea, però us estalvieu copiar-los i a més a més
-tindreu totes les notes que volgueu.
+Pot ser útil a l'inici de la pràctica, quan s'ha de crear els sintetitzadors. A la part on se us demana que
+escriviu un per un els cicles necessàris per fer cada nota.
+
+Si executeu aquest fitxer us crearà totes les notes possibles amb la llargada de la senyal de cicles que utilitzeu (de 0 a 127).
+Us serà útil perquè no ho haureu de copiar tot a mà i a més a més tindreu més notes de les que hi ha al fitxer que us dóna el professor.
 """
 variableCiclesNecessaris="ciclesNeed"
 liniaDefault = "\tto_unsigned({0}, {1}'length) when {2},"
